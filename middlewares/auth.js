348ab0f0dc30d27dcc8757fb26da8f5f3f778598
윤철중 <cjfwnd123zz@gmail.com>
2021-08-15 05:30:33 +0900

    add: 채팅방 기능

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,11 +5,3 @@ exports.isAuthenticated = (req, res, next) => {
     return res.send('<script>alert("로그인하신 뒤 다시 이용해주세요."); location.href="/auth/login";</script>');
   }
 };
-
-exports.isAdmin = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    if (req.user.isAdmin) next();
-  } else {
-    return res.send('<script>alert("접근 권한이 없습니다.");');
-  }
-};
