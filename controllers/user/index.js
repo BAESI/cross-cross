3e9382183b7f9a718c7d5118e6f8319b272271f3
윤철중 <cjfwnd123zz@gmail.com>
2021-08-14 16:22:27 +0900

    add: 프로필 수정하기

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const ctrl = require("./user.ctrl");
+const multer = require("../../multer");
 /**
  * @route GET /user
  * @description 마이프로필 조회
@@ -16,7 +17,15 @@ router.get("/edit", ctrl.get_user_edit);
 /**
  * @route POST /user/edit
  * @description 마이프로필 수정완료
+ * @body nickname, password
+ * @file image
  */
-router.post("/edit", ctrl.post_user_edit);
+router.post("/edit", multer.profileUpload.single("image"), ctrl.post_user_edit);
+
+/**
+ * @route PUT /user/edit/image
+ * @description 프로필 이미지 기본이미지로 변경
+ */
+router.put("/edit/image", ctrl.put_user_edit_image);
 
 module.exports = router;
